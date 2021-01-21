c5dafc65491f75e395fa189ae9fb4d059d0d0e17
Mahian <mdmahian580@gmail.com>
2021-01-21 13:20:30 +0600

    user post lists added with proper links

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
+    path('post_lists/<str:username>/', UserPostListView.as_view(), name='post-lists'),
     path('post_detail/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post_create/NEW/', PostCreateView.as_view(), name='post-create'),
     path('post_update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
