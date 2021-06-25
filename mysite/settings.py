8afa323f166cace561f3eb5c0da937462d7954f0
Mahian <mdmahian580@gmail.com>
2021-06-25 16:34:32 +0600

    reset password implemented

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,3 +114,10 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
